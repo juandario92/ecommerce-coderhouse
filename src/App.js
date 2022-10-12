5b4019d51becd62462c39a4ae2dbd44db85640bf
juandario92 <juandariorojas1992@gmail.com>
2022-10-11 22:59:42 -0300

    Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={"https://adidas.mx/glass/react/c478798/assets/img/icon-adidas-logo.svg"} className="header__logo" alt="logo" />
+        <div className='header__nav'>
+          <h3>Hombre</h3>
+          <h3>Mujer</h3>
+          <h3>Niños</h3>
+        </div>
+        <div className='header__buttons'>
+          <button>Carrito</button>
+        </div>
       </header>
+      <main className='products'>
+        <div className='card'>
+          <span className='card__name'> Zapato Deportivo</span>
+          <p className='card__description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque quod, accusantium iure maiores repellat corrupti?</p>
+          <span>$55</span>
+          <span>Quedan 5</span>
+        </div>
+        <div className='card'>
+          <span className='card__name'> Zapato Deportivo</span>
+          <p className='card__description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque quod, accusantium iure maiores repellat corrupti?</p>
+          <span>$55</span>
+          <span>Quedan 5</span>
+        </div>
+        <div className='card'>
+          <span className='card__name'> Zapato Deportivo</span>
+          <p className='card__description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque quod, accusantium iure maiores repellat corrupti?</p>
+          <span>$55</span>
+          <span>Quedan 5</span>
+        </div>
+        <div className='card'>
+          <span className='card__name'> Zapato Deportivo</span>
+          <p className='card__description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque quod, accusantium iure maiores repellat corrupti?</p>
+          <span>$55</span>
+          <span>Quedan 5</span>
+        </div>
+      </main>
+      <footer className='footer'>
+        <h3 className='footer__item'>Politica de privacidad</h3>
+        <h3 className='footer__item'>Terminos y Condiciones</h3>
+        <h3 className='footer__item'>Otra Cosa</h3>
+      </footer>
     </div>
   );
 }
